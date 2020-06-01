699775847a4ce1bbaab68339ccc22cff1729d47d
yuli19991001 <yuli19991001yuli@gmail.com>
2020-06-01 19:36:02 +0800

    Multiply Conpleted

--- 20200601.cpp
+++ 20200601.cpp
@@ -11,11 +11,17 @@ int minus(int x,int y)
 	return x-y;
 }
 
+int multiply(int x,int y)
+{
+	return x*y;
+}
+
 int main()
 {
 	int x,y;
 	cin>>x>>y;
 	cout<<add(x,y)<<endl;
 	cout<<minus(x,y)<<endl;
+	cout<<multiply(x,y)<<endl;
 	return 0;
  } 
