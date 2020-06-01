7a13ac11c52417ec9803ec5e9af57946921e006b
yuli19991001 <yuli19991001yuli@gmail.com>
2020-06-01 19:29:39 +0800

    減法

--- 20200601.cpp
+++ 20200601.cpp
@@ -6,11 +6,16 @@ int add(int x,int y)
 	return x+y;
 }
 
+int minus(int x,int y)
+{
+	return x-y;
+}
+
 int main()
 {
 	int x,y;
 	cin>>x>>y;
 	cout<<add(x,y)<<endl;
-	
+	cout<<minus(x,y)<<endl;
 	return 0;
  } 
